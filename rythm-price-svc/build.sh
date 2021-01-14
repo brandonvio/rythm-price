ead6f41c7cdd73f006e8dcfbe9116362be7973af
Brandon Vicedomini <brandonvio@outlook.com>
2021-01-13 22:44:59 -0800

    Added build file for Oanda. Beginning of refactor.

--- rythm-price-svc/build.sh
+++ rythm-price-svc/build.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-cd rythm-price-micro-serv
-./build.sh
-
-cd ..
-
-cd rythm-micro-serv-cdk
-npm install
-npm run build
-cdk synth "*"
-cdk deploy "*"
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 778477161868.dkr.ecr.us-west-2.amazonaws.com
+docker build -f DockerfileOanda  -t rythm-svc-oanda .
